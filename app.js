4b124ecd45e9cc15a828f63c0c589e4fd0c7aaa9
Fernando Faria <fernandoafaria@yahoo.com>
2018-09-01 19:41:36 -0400

    Shape creater

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+//*****Variables*****//
+
+//display form data//
+let objName = document.getElementById('name');
+let objWidth = document.getElementById('width');
+let objHeight = document.getElementById('height');
+let objRadius = document.getElementById('radius');
+let objArea = document.getElementById('area');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
