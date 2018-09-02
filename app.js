efa79e028329b6fbe0ff97fc3b2b0ecf44a082b1
Fernando Faria <fernandoafaria@yahoo.com>
2018-09-01 23:29:26 -0400

    work in progress

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-document.addEventListener('DOMContentLoaded', function(){
+//*******Classes********/
+
+
+
+class Shape {
+    constructor(height, width, xPos, yPos) {
+        this.height = height;
+        this.width = width;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+    displayInfo() {
+        objWidth.value = this.width;
+        objHeight.value = this.height;
+        objParimeter.value = (this.height + this.width) * 2;
+        objName.value = this.name;
+    }
+
+
+}
+
+class Circle extends Shape {
+    constructor(height, width, xPos, yPos, radius){
+        super(height, width, xPos, yPos)
+            this.radius = radius;
+            this.name = 'circle'
+
+    }
+
+
+}
+
+
+
 
 //*****Variables*****//
 
@@ -8,6 +41,34 @@ let objWidth = document.getElementById('width');
 let objHeight = document.getElementById('height');
 let objRadius = document.getElementById('radius');
 let objArea = document.getElementById('area');
+let objParimeter = document.getElementById('parimeter');
+
+//create buttons//
+
+let insertRec = document.getElementById('insertRec');
+let insertSq = document.getElementById('insertSq');
+let insertCir = document.getElementById('insertCir');
+let insertTri = document.getElementById('insertTri')
+
+
+//Insertion Values//
+let recWidth = document.getElementById('recWidth');
+let recHeight = document.getElementById('recHeight');
+
+let sqWidth = document.getElementById('sqWidth');
+let sqHeight = document.getElementById('sqHeight');
+
+let cirRadius = document.getElementById('cirRadius');
+
+let triHeight = document.getElementById('triHeight');
+
+
+//*****Generate Random POS *****/
+function randNum(min, max){
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+let square = new Circle();
 
 
 
@@ -30,15 +91,3 @@ let objArea = document.getElementById('area');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-})
